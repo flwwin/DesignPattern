ca5afb3c26f44a117dc5f9645147d923ed418209
flw <879465780@qq>
2020-05-30 13:24:28 +0800

    代理模式

--- src/com/company/proxy/TestUse.java
+++ src/com/company/proxy/TestUse.java
@@ -1,9 +1,17 @@
 package com.company.proxy;
+
+/**
+ * 代理模式，不直接访问对象通过代理对象访问。在代理对象中间层中可以加入需求的操作
+ *
+ */
 public class TestUse {
 	public static void main(String args[]){
-		AbstractObject obj = new ProxyObject();
-        obj.method1();
-        obj.method2();
-        obj.method3();
-	}
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        // 图像将从磁盘加载
+        image.display();
+        System.out.println("");
+        // 图像不需要从磁盘加载
+        image.display();
+    }
 }
